47fa9e5d5c82f6ec73d892cb71349e132199ce91
Brandon <bliberto@uci.edu>
2022-09-24 13:38:12 -0700

    Fixed API KEY ISSUES

--- src/Keys.jsx
+++
@@ -1,3 +0,0 @@
-export const API_KEY = "AIzaSyDwaugCkeKueYQBUHI8TTTdqZ87A568hgo"
-
-export default API_KEY
--- src/useGoogleSearch.jsx
+++ src/useGoogleSearch.jsx
@@ -1,21 +1,23 @@
-import {useState, useEffect} from 'react'
-import API_KEY from './Keys'
+import { useState, useEffect } from "react";
 
-const CONTEXT_KEY = "d09f9ab152aeb456e"
+const CONTEXT_KEY = "d09f9ab152aeb456e";
+const API_KEY = process.env.API_KEY;
 
 export default function useGoogleSearch(term) {
-    const [data, setData] = useState(null)
-    useEffect(() => {
-        const fetchData = async() => {
-            fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`)
-            .then(response => response.json())
-            .then(result => {
-                setData(result)
-            })
-        }
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          setData(result);
+        });
+    };
 
-        fetchData()
-    }, [term]) 
+    fetchData();
+  }, [term]);
 
-    return { data }
+  return { data };
 }
