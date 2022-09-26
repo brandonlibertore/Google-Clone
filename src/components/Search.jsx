e3532b8a5ef3f91821c2dbbf637783304ec2768a
Brandon <bliberto@uci.edu>
2022-09-26 00:52:55 -0700

    Made Google Clone Responsive.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,47 +1,64 @@
-import React, { useState } from 'react'
-import "./Search.css"
-import SearchIcon from '@mui/icons-material/Search';
-import MicIcon from '@mui/icons-material/KeyboardVoice';
-import Button from '@mui/material/Button';
-import { useNavigate } from "react-router-dom"
-import { useStateValue } from '../StateProvider';
-import { actionsTypes } from '../Reducer';
+import React, { useState } from "react";
+import "./Search.css";
+import SearchIcon from "@mui/icons-material/Search";
+import MicIcon from "@mui/icons-material/KeyboardVoice";
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
+import { useStateValue } from "../StateProvider";
+import { actionsTypes } from "../Reducer";
 
 export default function Search({ hideButtons = false }) {
-const [{}, dispatch] = useStateValue()
+  const [{}, dispatch] = useStateValue();
 
-    const [input, setInput] = useState("")
-    const navigate = useNavigate()
+  const [input, setInput] = useState("");
+  const navigate = useNavigate();
 
-    const search = event => {
-        event.preventDefault()
+  const search = (event) => {
+    event.preventDefault();
 
-        // DO SOMETHING WITH INPUT
-        navigate('/search')
-        dispatch({
-            type: actionsTypes.SET_SEARCH_TERM,
-            term: input
-        })
-    };
+    // DO SOMETHING WITH INPUT
+    navigate("/search");
+    dispatch({
+      type: actionsTypes.SET_SEARCH_TERM,
+      term: input,
+    });
+  };
 
   return (
-    <form className='search'>
-        <div className="search__input">
-            <SearchIcon className="search__input--icon" />
-            <input type="text" value={input} onChange={event => setInput(event.target.value)}/>
-            <MicIcon />
+    <form className="search">
+      <div className="search__input">
+        <SearchIcon className="search__input--icon" />
+        <input
+          type="text"
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+        <div className="mic__icon">
+          <MicIcon />
         </div>
-        {!hideButtons ? 
+      </div>
+      {!hideButtons ? (
         <div className="search__buttons">
-            <Button type="submit" onClick={search} variant="outlined">Google Search</Button>
-            <Button variant="outlined">I'm Feeling Lucky</Button>
+          <Button type="submit" onClick={search} variant="outlined">
+            Google Search
+          </Button>
+          <Button variant="outlined">I'm Feeling Lucky</Button>
         </div>
-        :
+      ) : (
         <div className="search__buttons">
-        <Button className="search__buttons--hidden" type="submit" onClick={search} variant="outlined">Google Search</Button>
-        <Button className="search__buttons--hidden" variant="outlined">I'm Feeling Lucky</Button>
-    </div>
-        }
+          <Button
+            className="search__buttons--hidden"
+            type="submit"
+            onClick={search}
+            variant="outlined"
+          >
+            Google Search
+          </Button>
+          <Button className="search__buttons--hidden" variant="outlined">
+            I'm Feeling Lucky
+          </Button>
+        </div>
+      )}
     </form>
-  )
+  );
 }
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import {StateProvider} from './StateProvider';
 import reducer, {initialState} from "./Reducer"
 
+console.log(process.env);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
